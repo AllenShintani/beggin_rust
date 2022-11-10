b301cccf9fec2dd9f9034ee12485556545710232
konjikun <107373499+konjikun@users.noreply.github.com>
2022-11-10 14:18:39 +0900

    数当てゲーム完成

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,20 +6,29 @@ fn main() {
     println!("Guess the number!"); //数を当ててみ
 
     let secret_number = rand::thread_rng().gen_range(1..101);
+    loop {
+        println!("Please input your guess."); //予想書いてねはあと
 
-    println!("Please input your guess."); //予想書いてねはあと
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin() //std::io::stdinでもおｋ
+            .read_line(&mut guess) //.がメソッドを表す
+            .expect("Failed to read line"); //行の読み込みに失敗しました。
 
-    io::stdin() //std::io::stdinでもおｋ
-        .read_line(&mut guess) //.がメソッドを表す
-        .expect("Failed to read line"); //行の読み込みに失敗しました。
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("You guessed: {}", guess); //次のように予想しました：{}
+        println!("You guessed: {}", guess); //次のように予想しました：{}
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
